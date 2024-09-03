f0fc392f6a8f2e6dc2d1c889a4a12dac9dc22625
philomagi <tooppoo56@gmail.com>
2024-09-03 16:57:46 +0900

    fix scripts

--- scripts/common/dist-path.ts
+++ scripts/common/dist-path.ts
@@ -8,6 +8,6 @@ export function distPath(target: Target, fileName: string): string {
   const here = fileURLToPath(import.meta.url)
   const dir = path.dirname(here)
 
-  return path.resolve(dir, '..', target, fileName)
+  return path.resolve(dir, '..', '..', target, fileName)
 }
 
