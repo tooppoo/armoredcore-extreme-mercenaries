f90458191bca3faa4e661408b03e140a9521f25d
philomagi <tooppoo56@gmail.com>
2025-01-17 23:46:11 +0900

    余計なログを削除

--- packages/front/app/lib/utils/highlight.ts
+++ packages/front/app/lib/utils/highlight.ts
@@ -3,10 +3,4 @@ import _diff from 'highlight.js/lib/languages/diff';
 
 hljs.registerLanguage('diff', _diff);
 
-export const diff = (src: string) => {
-  const r = hljs.highlight(src, { language: 'diff' }).value;
-
-  console.log(r)
-
-  return r
-}
+export const diff = (src: string) => hljs.highlight(src, { language: 'diff' }).value
