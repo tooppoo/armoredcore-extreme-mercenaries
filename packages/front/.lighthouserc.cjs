8e3c8d694303e1c772b7d6f37fdffb246e99e834
philomagi <tooppoo56@gmail.com>
2025-02-11 03:02:10 +0900

    lighthouse設定
    wrangler pages devでローカル起動してくれないので、npm run devで代用

---
+++ packages/front/.lighthouserc.cjs
@@ -0,0 +1,23 @@
+module.exports = {
+  ci: {
+    collect: {
+      url: [
+        'http://localhost:5173/',
+        'http://localhost:5173/rule',
+        'http://localhost:5173/penalties',
+        'http://localhost:5173/updates',
+        'http://localhost:5173/updates/b4c4b1e4-3e1b-1c3b-0f3d-3d1e2b4c1b4e',
+        'http://localhost:5173/archives',
+        'http://localhost:5173/archives/video',
+        'http://localhost:5173/archives/challenge',
+      ],
+      startServerCommand: 'npm run dev',
+    },
+    assertions: {
+      preset: 'lighthouse:recommended'
+    },
+    upload: {
+      target: 'temporary-public-storage',
+    },
+  },
+};
